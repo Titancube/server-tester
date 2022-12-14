776bfecd724658c906dccb4b8219d3b1eaba817e
Gyubin Lee <kbin0717@gmail.com>
2022-12-14 15:52:32 +0900

    fix: set no exec and add comments

--- container-check.sh
+++ container-check.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
-
+echo "# Checking if docker container exists"
 if [ "$(docker container inspect -f '{{.State.Status}}' server-tester)" == "true" ];
 then
+  echo # ⤷ ⚠️ Found container and trying to stop the process
   docker rm -f server-tester
+  echo # ⤷ ✅ Successfully stopped the process
 fi
 
+echo # ⚒️ Composing new container
 docker compose up -d
+echo # ✅️ New container is up and running
