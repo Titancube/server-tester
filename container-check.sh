d1b73c5b6478332d1b0d3fd5368b9814ddbaf19a
Gyubin Lee <kbin0717@gmail.com>
2022-12-14 16:51:06 +0900

    fix: update to newest image

--- container-check.sh
+++ container-check.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-echo "# Checking if docker container exists"
-if [ "$(docker container inspect -f '{{.State.Status}}' server-tester)" = "true" ]
-then
-  echo # ⤷ ⚠️ Found container and trying to stop the process
-  docker rm -f server-tester
-  echo # ⤷ ✅ Successfully stopped the process
-fi
-
-echo # ⚒️ Composing new container
-docker compose up -d
-echo # ✅️ New container is up and running
+echo "# ⟳ Updating to latest image"
+docker compose pull
+echo "# ⚒️ Composing new container"
+docker compose up -d --no-deps --build server-tester
+echo "# ✅️ New container is up and running"
