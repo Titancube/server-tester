106fab5adfe8971e99f36ff6b380d5efff78fd47
Gyubin Lee <kbin0717@gmail.com>
2022-12-14 16:30:00 +0900

    fix: sh

--- container-check.sh
+++ container-check.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "# Checking if docker container exists"
-if [ "$(docker container inspect -f '{{.State.Status}}' server-tester)" == "true" ];
+if [ "$(docker container inspect -f '{{.State.Status}}' server-tester)" = "true" ];
 then
   echo # ⤷ ⚠️ Found container and trying to stop the process
   docker rm -f server-tester
