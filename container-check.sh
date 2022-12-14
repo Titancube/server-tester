cc0a0fe5348c87ce15e7877bfdf8dc0f03975e39
Gyubin Lee <kbin0717@gmail.com>
2022-12-14 15:12:26 +0900

    fix: container script

--- container-check.sh
+++ container-check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/zsh
 
-if [ "$(docker container inspect -f '{{.State.Status}}') server-tester" == "true" ];
+if [ "$(docker container inspect -f '{{.State.Status}}' server-tester)" == "true" ];
 then
   docker rm -f server-tester
 fi
